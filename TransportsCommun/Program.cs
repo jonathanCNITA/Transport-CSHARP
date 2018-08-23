c1d5bd9b9cb90b72b08a9b7a2498eb1f27b149b1
Jonathan Courat <jonathan.courat@campus-numerique-in-the-alps.com>
2018-08-23 11:59:56 +0200

    Set a request and log the response

--- TransportsCommun/Program.cs
+++ TransportsCommun/Program.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.IO;
+using Newtonsoft.Json.Linq;
 
 namespace TransportsCommun
 {
@@ -11,6 +16,24 @@ namespace TransportsCommun
         static void Main(string[] args)
         {
             Console.WriteLine("Starting Common transport project");
+
+            //-- Position du campus: y:45.18549, x:5.72792
+            string urlApi = "http://data.metromobilite.fr/api/linesNear/json?x=5.72792&y=45.18549&dist=800&details=true";
+
+            WebRequest request = WebRequest.Create(urlApi);
+            WebResponse response = request.GetResponse();
+            Console.WriteLine("Reponse status : " + ((HttpWebResponse)response).StatusDescription);
+
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd();
+            
+            Console.WriteLine("------------------------------");
+            Console.WriteLine(responseFromServer);
+            reader.Close();
+            response.Close();
+
+            Console.ReadLine();
         }
     }
 }
